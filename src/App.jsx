97eb755bc7f2f2ce9c2331606d19d50272b38a51
AFDtea <77896483+AFDtea@users.noreply.github.com>
2024-10-08 11:53:22 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Suspense } from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
-import AdvisorsPage from './pages/advisors/AdvisorsPage';
+import AdvisorsPage from './pages/advisors/PeerRec';
 import Welcome from './pages/welcome';
 import MovieRatingPage from './pages/ratemovies/MovieRatingPage';
 import Survey from './pages/survey';
@@ -17,7 +17,7 @@ function App() {
       <header className="App-header">
         <h1>Preference Community</h1>
       </header>
-      <Router basename='/preference-community'>
+      <Router basename='/peer-recommendation-platform'>
         <Suspense fallback={<div>Loading...</div>}>
           <Routes>
             <Route path="/" element={<Welcome next="/presurvey"/>} />
@@ -25,7 +25,7 @@ function App() {
             <Route path="/presurvey" element={<Survey next="/systemintro"/>} /> 
             <Route path="/systemintro" element={<SystemIntro next="/ratemovies"/>} />
             <Route path="/ratemovies" element={<MovieRatingPage next="/advisors"/>} />
-            <Route path="/advisors" element={<AdvisorsPage next="/postsurvey"/>} />
+            <Route path="/peer-recommendations" element={<AdvisorsPage next="/postsurvey"/>} />
             <Route path="/postsurvey" element={<PostSurvey next="/feedback"/>} />
             <Route path="/feedback" element={<FeedbackPage next="/quit"/>} />
             <Route path="/quit" element={<h1>Thank you for participating!</h1>} />
